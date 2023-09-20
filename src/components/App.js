199ff06f1fdd6f359f6e5e57b2786afd2fe75e63
choikkk <sixballoons@naver.com>
2023-09-20 16:19:50 +0900

    230920

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import Editor from "./Editor.js";
+import Sidebar from "./Sidebar.js";
+
+export default function App ({ $target }) {
+    const initialState = []
+    new Sidebar({ $target, initialState })
+    new Editor({ $target })
+}
