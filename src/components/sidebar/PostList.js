46edf40a89d541906ed15140861f8b5d64093045
choikkk <sixballoons@naver.com>
2023-09-26 14:19:49 +0900

    230926

---
+++ src/components/sidebar/PostList.js
@@ -0,0 +1,38 @@
+export default function PostList({$target, testData}) {
+    const $postList = document.createElement("div")
+    $postList.className = "postList"
+    $target.appendChild($postList)
+    
+    this.createTreeView = (data) => {
+        let str = ''
+        for (const key in data) {
+            if (data[key].documents.length > 0) {
+                str += `<li class="dataList">📄 ${data[key].title}
+                    <button class="addBtn">➕</button>
+                    <button class="delBtn">🗑️</button>
+                    <ul>${this.createTreeView(data[key].documents)}</ul>
+                </li>`
+            } else {
+                str += `<li class="dataList">📄 ${data[key].title}
+                    <button class="addBtn">➕</button>
+                    <button class="delBtn">🗑️</button>
+                </li>` 
+            }
+        }
+
+        return str
+    }
+
+    $postList.innerHTML = 
+        `<ul>
+            ${testData.map(document => 
+                    `<li class="dataList">📄 ${document.title}
+                        <button class="addBtn">➕</button>
+                        <button class="delBtn">🗑️</button>
+                    </li>
+                    ${document.documents.length > 0 ? `<ul>${this.createTreeView(document.documents)}</ul>` : ''}
+                    `
+                ).join("")
+            }
+        </ul>`
+    } 
---
+++ src/components/sidebar/PostPage.js
@@ -0,0 +1,49 @@
+import { request } from "../../utils/api.js"
+import PostList from "./PostList.js"
+
+export default function PostPage ({ $target }) {
+    const $page = document.createElement("div")
+    $page.className = 'documentDiv'
+    $target.appendChild($page)
+
+    // api 
+    const data = request("/documents")
+
+    const testData = [
+        {
+          "id": 1, // Document id
+          "title": "노션을 만들자", // Document title
+          "documents": [
+            {
+              "id": 2,
+              "title": "블라블라",
+              "documents": [
+                {
+                  "id": 3,
+                  "title": "함냐함냐",
+                  "documents": []
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "id": 4,
+          "title": "hello!",
+          "documents": []
+        }
+    ]
+
+    new PostList({ 
+      $target: $page,
+      testData
+    })
+
+    // TODO: 컴포넌트화 시키기
+    const $newBtn = document.createElement("button")
+    $newBtn.textContent = "+ New Page"
+    $newBtn.className = "addNew"
+    $page.appendChild($newBtn)
+  
+    // this.state = testData
+}
